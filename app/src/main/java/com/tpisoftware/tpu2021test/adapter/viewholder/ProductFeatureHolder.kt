41d04510bf6e0e84ac42e8faed4989dc4527eef9
Fox <fox.tsai@tpisoftware.com>
2021-06-01 09:49:50 +0800

    fix viewholder

--- app/src/main/java/com/tpisoftware/tpu2021test/adapter/viewholder/ProductFeatureHolder.kt
+++ app/src/main/java/com/tpisoftware/tpu2021test/adapter/viewholder/ProductFeatureHolder.kt
@@ -2,10 +2,11 @@ package com.tpisoftware.tpu2021test.adapter.viewholder
 
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.tpisoftware.tpu2021test.R
 
 class ProductFeatureHolder(v: View) : RecyclerView.ViewHolder(v) {
     val featureIcon: ImageView = v.findViewById(R.id.feature_icon)
-    val featureText: ImageView = v.findViewById(R.id.feature_text)
+    val featureText: TextView = v.findViewById(R.id.feature_text)
 }
